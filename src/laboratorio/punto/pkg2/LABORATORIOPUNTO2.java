f99c0138e72711de6640756cb3b31a1bb91d5676
lenovo <lenovo@DESKTOP-REKBERN>
2017-09-09 18:28:23 -0500

    PUNTO 3 LABORATORIO

--- src/laboratorio/punto/pkg2/LABORATORIOPUNTO2.java
+++ src/laboratorio/punto/pkg2/LABORATORIOPUNTO2.java
@@ -6,6 +6,13 @@ public class LABORATORIOPUNTO2 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
+        System.out.println("************ BIENVENIDO A TU TIENDA VIRTUAL ************");
+        System.out.println("Porfavor ingrese los datos de la tienda:");
+        System.out.println("Ingrese el Nombre de la Tienda y el Nombre del Dueño:");
+        String Nombre_Tienda = in.nextLine();
+        String Nombre_Dueño = in.nextLine();
+        Tienda Tienda1 = new Tienda (Nombre_Tienda, Nombre_Dueño);
 
     }
 
